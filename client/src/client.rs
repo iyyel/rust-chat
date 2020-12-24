2bc622932f250e10574959a6b0daeb13fa4763ce
iyyel <i@iyyel.io>
2020-12-24 16:04:08 +0100

    Name assignment, private messages, peer data now working.

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,5 +1,7 @@
 use futures::{future, pin_mut, StreamExt};
 
+use std::collections::HashSet;
+
 use async_std::io;
 use serde::{Deserialize, Serialize};
 
@@ -11,7 +13,7 @@ use async_tungstenite::tungstenite::protocol::Message as TungMessage;
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Message {
     src_addr: String,
-    name: String,
+    src_name: String,
     msg_type: MessageType,
     msg: String,
 }
@@ -21,60 +23,70 @@ enum MessageType {
     NewPeer(String), // Broadcast this message to all peers when a new peer has connected. The parameter is the name of the peer that has connected.
     LostPeer(String), // Broadcast this message to all peers when a peers has disconnected. The parameter is the name of the peer that has disconnected.
     Text,             // Standard peer text message (broadcasted message)
-    PeerData, // If received by the server, a peer is asking to retrieve data about all connected peers. If received by a peer, it is the data from the server.
+    PeerDataRequest,
+    PeerDataReply(PeerData), // If received by the server, a peer is asking to retrieve data about all connected peers. If received by a peer, it is the data from the server.
     PeerName(String), // The server sends this message to a client when it has first connected, giving it a random name. The name is the parameter.
     Private(String),  // A private message to the given name of a peer.
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct PeerData {
+    count: i32,
+    peer_names: HashSet<String>
+}
+
 pub struct Client {
     addr: String,
-    name: String
+    name: String,
 }
 
 impl Client {
     pub fn new(addr: String) -> Self {
-        Self { addr, name: String::new() }
+        Self {
+            addr,
+            name: String::new(),
+        }
     }
 
     pub async fn connect(&mut self) {
-
         let (sender, receiver) = futures::channel::mpsc::unbounded::<TungMessage>();
 
         let (ws_stream, _) = connect_async(format!("ws://{}/socket", &self.addr))
             .await
             .expect("Failed to connect");
 
-        println!("WebSocket handshake has been successfully completed");
+        println!("WebSocket handshake has been successfully completed.");
 
         let local_addr = ws_stream.get_ref().local_addr().unwrap().to_string();
 
         let (write, mut read) = ws_stream.split();
 
         let stdin_to_ws = receiver.map(Ok).forward(write);
-        
-        let mut name = String::new();
 
-        // read first single message for name
-        if let Some(msg) = read.next().await {
-            let msg = msg.unwrap().to_string();
-            let msg: Message = serde_json::from_str(&msg).unwrap();
-            let msg_type = msg.msg_type.clone();
+        // Wait until name message has been received.
+        loop {
+            if let Some(msg) = read.next().await {
+                let msg = msg.unwrap().to_string();
+                let msg: Message = serde_json::from_str(&msg).unwrap();
+                let msg_type = msg.msg_type.clone();
 
-            match msg_type {
-                MessageType::PeerName(new_name) => {
-                    async_std::io::stdout()
-                        .write_all(format!("\nName Assignment: {}", new_name).as_bytes())
-                        .await
-                        .unwrap();
-                        name = new_name;
+                match msg_type {
+                    MessageType::PeerName(new_name) => {
+                        async_std::io::stdout()
+                            .write_all(format!("\nWelcome to Rust-Chat, {}!", new_name).as_bytes())
+                            .await
+                            .unwrap();
+                        self.name = new_name;
+                        async_std::io::stdout().flush().await.unwrap();
+                        break;
+                    }
+                    _ => continue,
                 }
-                _ => {}
-            }
-            async_std::io::stdout().flush().await.unwrap();
-        };
+            };
+        }
 
-        task::spawn(read_stdin(sender, local_addr, name));
-        
+        task::spawn(read_stdin(sender, local_addr, self.name.clone()));
+
         let ws_to_stdout = async {
             while let Some(msg) = read.next().await {
                 let msg = msg.unwrap().to_string();
@@ -82,57 +94,49 @@ impl Client {
                 let msg_type = msg.msg_type.clone();
 
                 match msg_type {
-                    MessageType::NewPeer(peer_n) => async_std::io::stdout()
+                    MessageType::NewPeer(peer_name) => async_std::io::stdout()
                         .write_all(
-                            format!("\n New Client: {}: {:?}", &msg.src_addr, &msg).as_bytes(),
+                            format!("\n{}: {} has connected.", &msg.src_name, peer_name).as_bytes(),
                         )
                         .await
                         .unwrap(),
-                    MessageType::LostPeer(peer_n) => async_std::io::stdout()
+                    MessageType::LostPeer(peer_name) => async_std::io::stdout()
                         .write_all(
-                            format!("\nClient disconnected: {}: {:?}", &msg.src_addr, &msg)
+                            format!("\n{}: {} has disconnected.", &msg.src_name, peer_name)
                                 .as_bytes(),
                         )
                         .await
                         .unwrap(),
                     MessageType::Text => async_std::io::stdout()
-                        .write_all(format!("\nText: {}: {:?}", &msg.src_addr, &msg).as_bytes())
+                        .write_all(format!("\n{}: {}", &msg.src_name, &msg.msg).as_bytes())
                         .await
                         .unwrap(),
-                    MessageType::PeerData => async_std::io::stdout()
-                        .write_all(
-                            format!("\nPeer data: {}: {:?}", &msg.src_addr, &msg).as_bytes(),
-                        )
+                    MessageType::PeerDataRequest => async_std::io::stdout()
+                        .write_all(format!("\n{}: {}", &msg.src_name, &msg.msg).as_bytes())
                         .await
                         .unwrap(),
-                    MessageType::PeerName(new_name) => {
-
-    
-                        self.name = new_name.clone();
-                        
+                    MessageType::PeerDataReply(peer_data) => async_std::io::stdout()
+                        .write_all(format!("\n{}: {:?}", &msg.src_name, peer_data).as_bytes())
+                        .await
+                        .unwrap(),
+                    MessageType::PeerName(name) => {
                         async_std::io::stdout()
-                        .write_all(format!("\nName Assignment: {}", new_name).as_bytes())
-                        .await
-                        .unwrap();
-                    },
-                    MessageType::Private(_) => async_std::io::stdout()
+                            .write_all(
+                                format!("\n{}: {}, {}", &msg.src_name, &msg.msg, name).as_bytes(),
+                            )
+                            .await
+                            .unwrap();
+                    }
+                    MessageType::Private(name) => async_std::io::stdout()
                         .write_all(
-                            format!("\n Private message: {}: {:?}", &msg.src_addr, &msg).as_bytes(),
+                            format!("\n{}: {}: {}", &msg.src_name, &msg.msg, name).as_bytes(),
                         )
                         .await
                         .unwrap(),
                 }
-
                 async_std::io::stdout().flush().await.unwrap();
             }
         };
-
-/*
-        let ws_to_stdout = {
-            read.for_each(|message| async {
-                
-            })
-        };*/
 
         pin_mut!(stdin_to_ws, ws_to_stdout);
         future::select(stdin_to_ws, ws_to_stdout).await;
@@ -144,10 +148,10 @@ impl Client {
 async fn read_stdin(
     sender: futures::channel::mpsc::UnboundedSender<TungMessage>,
     local_addr: String,
-    peer_name: String
+    peer_name: String,
 ) {
     let mut stdin = io::stdin();
-    
+
     loop {
         async_std::io::stdout()
             .write_all(format!("\n{}: ", peer_name).as_bytes())
@@ -164,9 +168,41 @@ async fn read_stdin(
 
         let msg = String::from_utf8(buf).unwrap();
 
+        if msg.starts_with("pm: ") {
+            let split: Vec<&str> = msg.split(" ").collect();
+            let (recv_name, msg) = (split[1].to_string(), split[2].to_string());
+
+            let msg_struct = Message {
+                src_addr: local_addr.to_string(),
+                src_name: peer_name.to_string(),
+                msg_type: MessageType::Private(recv_name),
+                msg: msg,
+            };
+
+            sender
+                .unbounded_send(TungMessage::Text(
+                    serde_json::to_string(&msg_struct).unwrap(),
+                ))
+                .unwrap();
+        } else if msg.starts_with("peerdata: ") {
+            let msg_struct = Message {
+                src_addr: local_addr.to_string(),
+                src_name: peer_name.to_string(),
+                msg_type: MessageType::PeerDataRequest,
+                msg: String::from(""),
+            };
+
+            sender
+                .unbounded_send(TungMessage::Text(
+                    serde_json::to_string(&msg_struct).unwrap(),
+                ))
+                .unwrap();
+        
+        } else {
+
         let msg_struct = Message {
             src_addr: local_addr.to_string(),
-            name: peer_name.to_string(),
+            src_name: peer_name.to_string(),
             msg_type: MessageType::Text,
             msg: msg,
         };
@@ -176,5 +212,6 @@ async fn read_stdin(
                 serde_json::to_string(&msg_struct).unwrap(),
             ))
             .unwrap();
+        }
     }
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,8 +1,15 @@
 use rand::seq::SliceRandom;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::io::Error as IoError;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
+
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Write},
+};
+
+use std::iter::FromIterator;
 
 use serde::{Deserialize, Serialize};
 
@@ -16,14 +23,14 @@ use async_tungstenite::tungstenite::protocol::Message as TungMessage;
 
 type Sender = UnboundedSender<TungMessage>;
 type PeerMap = Arc<Mutex<HashMap<SocketAddr, Sender>>>;
-type PeerNameMap = Arc<Mutex<HashMap<SocketAddr, String>>>;
+type PeerNameMap = Arc<Mutex<HashMap<String, SocketAddr>>>;
 
 const NAME: &str = "Server";
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Message {
     src_addr: String,
-    name: String,
+    src_name: String,
     msg_type: MessageType,
     msg: String,
 }
@@ -33,21 +40,29 @@ struct Message {
 // Try to see if you can avoid using Ip's for communication to the peers.
 // If the peers only use communication with names, it is more secure and anonymous.
 //
+// If the reicpient of a private message does not exist, send back a message to the peer telling them that the name does not exist.
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 enum MessageType {
     NewPeer(String), // Broadcast this message to all peers when a new peer has connected. The parameter is the name of the peer that has connected.
     LostPeer(String), // Broadcast this message to all peers when a peers has disconnected. The parameter is the name of the peer that has disconnected.
     Text,             // Standard peer text message (broadcasted message)
-    PeerData, // If received by the server, a peer is asking to retrieve data about all connected peers. If received by a peer, it is the data from the server.
+    PeerDataRequest,
+    PeerDataReply(PeerData), // If received by the server, a peer is asking to retrieve data about all connected peers. If received by a peer, it is the data from the server.
     PeerName(String), // The server sends this message to a client when it has first connected, giving it a random name. The name is the parameter.
     Private(String),  // A private message to the given name of a peer.
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct PeerData {
+    count: i32,
+    peer_names: HashSet<String>,
 }
 
 pub struct Server {
     addr: String,
     peers: PeerMap,
-    peer_names: PeerNameMap,
+    peer_name_map: PeerNameMap,
 }
 
 impl Server {
@@ -55,7 +70,7 @@ impl Server {
         Self {
             addr,
             peers: PeerMap::new(Mutex::new(HashMap::new())),
-            peer_names: PeerNameMap::new(Mutex::new(HashMap::new())),
+            peer_name_map: PeerNameMap::new(Mutex::new(HashMap::new())),
         }
     }
 
@@ -65,14 +80,17 @@ impl Server {
         let listener = try_socket.expect("Failed to bind");
         println!("Listening on: {}", &self.addr);
 
+        let names = parse_names();
+
         // Let's spawn the handling of each connection in a separate task.
         while let Ok((stream, peer_addr)) = listener.accept().await {
             task::spawn(on_peer_connect(
                 self.peers.clone(),
-                self.peer_names.clone(),
+                self.peer_name_map.clone(),
                 stream,
                 self.addr.clone(),
                 peer_addr,
+                names.clone()
             ));
         }
 
@@ -82,10 +100,11 @@ impl Server {
 
 async fn on_peer_connect(
     peers: PeerMap,
-    peer_names: PeerNameMap,
+    peer_name_map: PeerNameMap,
     raw_stream: TcpStream,
     local_addr: String,
     peer_addr: SocketAddr,
+    names: HashSet<String>,
 ) {
     println!("\nIncoming TCP connection from: {}", peer_addr);
 
@@ -93,19 +112,27 @@ async fn on_peer_connect(
         .await
         .expect("Error during the websocket handshake occurred");
 
-    let mut names = vec!["Charles", "James"];
-    let peer_name = names.choose(&mut rand::thread_rng()).unwrap().to_string();
-    names.remove(names.iter().position(|x| *x == peer_name).unwrap());
+    let name_map = peer_name_map.lock().unwrap().clone();
+    let curr_names: HashSet<String> = name_map.keys().map(|k| k.to_string()).collect();
+    let available_names: Vec<String> = names.difference(&curr_names).map(|n| n.to_string()).collect();
+    let peer_name = available_names.choose(&mut rand::thread_rng()).unwrap().to_string();
+    let peer_spots_left = available_names.len() - 1;
+
+    // do something if there are no available names left!
+
+
+
 
     // bind the peer address to the given name such that we can remove it
     // later, but not re-use it while the peer is still active.
-    peer_names
+    peer_name_map
         .lock()
         .unwrap()
-        .insert(peer_addr, peer_name.clone());
+        .insert(peer_name.clone(), peer_addr);
 
     broadcast_new_peer_msg(&peers, &local_addr, &peer_addr, &peer_name);
     println!("{} ({}) has connected.", peer_name, peer_addr);
+    println!("Peer spots left: {}", peer_spots_left);
 
     let (sender, receiver) = unbounded();
     send_name_msg(&sender, &peer_name, &local_addr);
@@ -127,15 +154,58 @@ async fn on_peer_connect(
 
             match msg_type {
                 MessageType::Text => {
-                    println!("\n{}: {:?}", peer_addr, msg);
+                    println!("\n{} ({}): {}", msg.src_name, peer_addr, msg.msg);
                     broadcast_msg(&peers, &peer_addr, msg);
                 }
-                MessageType::PeerData => {
-                    println!("\nSending client data: {}: {:?}", peer_addr, msg);
+                MessageType::PeerDataRequest => {
+                    println!("\nSending peer data: {}: {:?}", peer_addr, msg);
+
+                    let peer_data = create_peer_data(&peer_name_map, &msg.src_name);
+
+                    let msg = Message {
+                        src_addr: local_addr.clone(),
+                        src_name: NAME.to_string(),
+                        msg_type: MessageType::PeerDataReply(peer_data.clone()),
+                        msg: String::from(""),
+                    };
+
+                    println!(
+                        "\n{} ({}) -> {} ({}): {:?}",
+                        NAME, local_addr, peer_name, peer_addr, peer_data
+                    );
+
+                    send_single_msg(&peers, &peer_addr, msg);
                 }
-                MessageType::Private(addr) => {
-                    let socket: SocketAddr = addr.clone().parse().unwrap();
-                    send_msg(&peers, &socket, msg);
+                MessageType::PeerDataReply(peer_data) => {
+                    println!("\nReciving peer data: {}: {:?}", peer_addr, msg);
+                }
+                MessageType::Private(recv_peer_name) => {
+                    if let Some(recv_peer_addr) =
+                        &peer_name_map.lock().unwrap().get(&recv_peer_name)
+                    {
+                        if recv_peer_name != peer_name {
+                            println!(
+                                "\n{} ({}) -> {} ({}): {}",
+                                msg.src_name, peer_addr, recv_peer_name, recv_peer_addr, msg.msg
+                            );
+
+                            send_single_msg(&peers, recv_peer_addr, msg);
+                        }
+                    } else {
+                        let msg = Message {
+                            src_addr: local_addr.clone(),
+                            src_name: NAME.to_string(),
+                            msg_type: MessageType::Private(peer_name.clone()),
+                            msg: format!("{} is not connected.", recv_peer_name),
+                        };
+
+                        println!(
+                            "\n{} ({}) -> {} ({}): {:?}",
+                            NAME, local_addr, peer_name, peer_addr, &msg.msg
+                        );
+
+                        send_single_msg(&peers, &peer_addr, msg);
+                    }
                 }
                 _ => (),
             }
@@ -148,15 +218,20 @@ async fn on_peer_connect(
     pin_mut!(broadcast_incoming, receive_from_others);
     future::select(broadcast_incoming, receive_from_others).await;
 
-    let peer_names_lock = peer_names.lock().unwrap();
-    let lost_name = peer_names_lock.get(&peer_addr).unwrap();
+    let mut lost_name = "Unknown".to_string();
 
-    broadcast_lost_peer_msg(&peers, &local_addr, &peer_addr, lost_name);
-    println!("{} ({}) has disconnected.", peer_name, peer_addr);
+    let mut peer_names = peer_name_map.lock().unwrap();
+    for key in peer_names.keys() {
+        if peer_names.get(key).unwrap() == &peer_addr {
+            lost_name = key.clone();
+            break;
+        }
+    }
 
-    names.push(lost_name);
-    peer_names.lock().unwrap().remove(&peer_addr);
+    peer_names.remove(&lost_name);
     peers.lock().unwrap().remove(&peer_addr);
+    broadcast_lost_peer_msg(&peers, &local_addr, &peer_addr, &lost_name);
+    println!("{} ({}) has disconnected.", peer_name, peer_addr);
 }
 
 fn broadcast_msg(peers: &PeerMap, peer_addr: &SocketAddr, msg: Message) {
@@ -174,7 +249,7 @@ fn broadcast_msg(peers: &PeerMap, peer_addr: &SocketAddr, msg: Message) {
     }
 }
 
-fn send_msg(peers: &PeerMap, peer_addr: &SocketAddr, msg: Message) {
+fn send_single_msg(peers: &PeerMap, peer_addr: &SocketAddr, msg: Message) {
     let peers = peers.lock().unwrap();
     let msg = TungMessage::Text(serde_json::to_string(&msg).unwrap());
 
@@ -191,7 +266,7 @@ fn broadcast_new_peer_msg(
 ) {
     let msg = Message {
         src_addr: local_addr.clone(),
-        name: NAME.to_string(),
+        src_name: NAME.to_string(),
         msg_type: MessageType::NewPeer(peer_name.clone()),
         msg: format!("{} ({}) has connected.", peer_name, peer_addr),
     };
@@ -207,7 +282,7 @@ fn broadcast_lost_peer_msg(
 ) {
     let msg = Message {
         src_addr: local_addr.clone(),
-        name: NAME.to_string(),
+        src_name: NAME.to_string(),
         msg_type: MessageType::LostPeer(peer_name.clone()),
         msg: format!("{} ({}) has disconnected.", peer_name, peer_addr),
     };
@@ -218,11 +293,45 @@ fn broadcast_lost_peer_msg(
 fn send_name_msg(sender: &Sender, peer_name: &String, local_addr: &String) {
     let msg = Message {
         src_addr: local_addr.clone(),
-        name: NAME.to_string(),
+        src_name: NAME.to_string(),
         msg_type: MessageType::PeerName(peer_name.clone()),
         msg: "Name message".to_string(),
     };
 
     let msg = TungMessage::Text(serde_json::to_string(&msg).unwrap());
     sender.unbounded_send(msg).unwrap();
+}
+
+fn create_peer_data(peer_name_map: &PeerNameMap, src_name: &String) -> PeerData {
+    let peer_names: HashSet<String> = peer_name_map
+        .lock()
+        .unwrap()
+        .keys()
+        .filter(|&k| k != src_name)
+        .map(|k| k.to_string())
+        .collect();
+
+    let count = peer_name_map.lock().unwrap().keys().len() as i32;
+
+    let peer_data = PeerData {
+        count,
+        peer_names: peer_names.clone(),
+    };
+
+    peer_data
+}
+
+fn parse_names() -> HashSet<String> {
+    let mut file = File::open("names.txt").expect("file error");
+    let reader = BufReader::new(&mut file);
+
+    let mut lines: Vec<_> = reader
+        .lines()
+        .map(|l| l.expect("Couldn't read a line"))
+        .collect();
+
+    lines.sort();
+    lines.dedup();
+
+    HashSet::from_iter(lines)
 }
